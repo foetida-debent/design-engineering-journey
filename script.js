14a93f2ad309f754aa7e8576a8edd90086a15cb3
Viktor <sharik6190@gmail.com>
2026-09-06 18:40:42 +0300

    add JS, dark/light theme toggle

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log ("JS парцює")
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', () => {
+  console.log('кнопку натиснули');
+});
+const themeToggle = document.querySelector('.theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('light');
+});
