043e380ace7f36e7b2cdcf94896908860367e453
Viktor <sharik6190@gmail.com>
2026-09-06 19:02:22 +0300

    added night icon

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-console.log ("JS парцює")
-const button = document.querySelector('.btn');
+console.log("JS працює");
 
+const button = document.querySelector('.btn');
 button.addEventListener('click', () => {
   console.log('кнопку натиснули');
 });
-const themeToggle = document.querySelector('.theme-toggle');
 
+const themeToggle = document.querySelector('.theme-toggle');
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('light');
+  
+  if (document.body.classList.contains('light')) {
+    themeToggle.textContent = '🌙';
+  } else {
+    themeToggle.textContent = '☀️';
+  }
 });
